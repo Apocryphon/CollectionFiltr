37a3d41c792c239d9d26e5e451e7f6665acebe25
Richard Yeh <apocryphon.now@gmail.com>
2016-10-22 14:25:13 -0700

    Initial Commit

---
+++ CollectionFiltr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionFiltr
+//
+//  Created by Richard Yeh on 10/22/16.
+//  Copyright © 2016 Richard Yeh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionFiltr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionFiltr
+//
+//  Created by Richard Yeh on 10/22/16.
+//  Copyright © 2016 Richard Yeh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
